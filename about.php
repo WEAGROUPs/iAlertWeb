fc7942d8f3006b549198efac8c22bf685cd2f7b3
WEA TECH GROUP <weatechgroup@gmail.com>
2014-01-17 09:43:42 -0800

    Update about.php

--- about.php
+++ about.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
 <meta charset ="utf-8">
+
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <link rel="stylesheet"  href="css/bootstrap.min.css" media="screen">
 <link rel="stylesheet" href="css/bootstrap-responsive.min.css" media="screen">
